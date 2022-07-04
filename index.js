5187967015d8a18aa215ecd893afed51813396e7
Alex Guzman <aguzman@buk.pe>
2022-07-04 01:34:24 -0500

    feat:Add phonebook step3

--- index.js
+++ index.js
@@ -36,6 +36,16 @@ app.get('/info', (request, response) => {
     )
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 
 app.listen(PORT, () => {
