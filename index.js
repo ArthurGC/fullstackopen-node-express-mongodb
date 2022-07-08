f66ab9ba98ab85453a4f84888ac6e76df5598a68
Alex Guzman <aguzman@buk.pe>
2022-07-08 02:22:50 -0500

    feat:Add phonebook step8

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ var morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (request) => JSON.stringify(request.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
